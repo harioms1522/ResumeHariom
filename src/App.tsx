97f24b06df8d92598b5713ca960bca43e1d5dd44
Hariom Sharma <hariom.sharma@zeno.health>
2025-04-07 21:05:08 +0530

    value not used

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Experience from './components/Experience';
 import Skills from './components/Skills';
-import Projects from './components/Projects';
+// import Projects from './components/Projects';
 import Contact from './components/Contact';
 
 const theme = createTheme({
