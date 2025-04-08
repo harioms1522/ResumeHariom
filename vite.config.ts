53d39a78334ff9484587d455ae36d9fb5cc827ef
Hariom Sharma <hariom.sharma@zeno.health>
2025-04-09 03:36:07 +0530

    Blogging Setup using MDX

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mdx from '@mdx-js/rollup'
 
 // https://vite.dev/config/
+
+declare module '*.mdx' {
+  let MDXComponent: (props: any) => JSX.Element
+}
+
 export default defineConfig({
   base: '/ResumeHariom/',
-  plugins: [react()],
+  plugins: [react(), , mdx()],
 })
