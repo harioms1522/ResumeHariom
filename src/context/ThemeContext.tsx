44ea60e983637e675a34483b26dfe4ec4cf409db
Hariom Sharma <harioms1522@gmail.com>
2025-09-29 17:04:15 +0530

    default background

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -56,7 +56,7 @@ const darkTheme = createTheme({
 });
 
 export const CustomThemeProvider = ({ children }: { children: ReactNode }) => {
-    const [themeMode, setThemeMode] = useState<'light' | 'dark'>('dark');
+    const [themeMode, setThemeMode] = useState<'light' | 'dark'>('light');
 
     const theme = useMemo(() =>
         themeMode === 'dark' ? darkTheme : lightTheme, [themeMode]
